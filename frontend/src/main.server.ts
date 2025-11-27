8d89de679dcf8048b64e744f2d59348702dc49e3
Sengtri Bun <sengtri457@gmail.com>
2025-11-27 20:34:46 +0700

    Fix frontend folder not showing as normal folder

---
+++ frontend/src/main.server.ts
@@ -0,0 +1,7 @@
+import { bootstrapApplication,BootstrapContext } from '@angular/platform-browser';
+import { App } from './app/app';
+import { config } from './app/app.config.server';
+
+export default (context : BootstrapContext)=>{
+    return bootstrapApplication(App,config, context);
+}
